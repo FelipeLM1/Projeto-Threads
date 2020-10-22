83532b54e0f9bd91fbb242471e4c7bd32dd7de71
Felipe Lima <felipeti@ufrn.edu.br>
2020-10-21 23:27:41 -0300

    matriz de 0 a 255

--- src/App.java
+++ src/App.java
@@ -12,19 +12,20 @@ public class App {
             BufferedImage img = ImageIO.read(new File(pathImg));
             int largura = img.getWidth();
             int altura = img.getHeight();
+            int[][] matrizCinza = new int[largura][altura];
             for (int i = 0; i < largura; i++) {
                 for (int j = 0; j < altura; j++) {
                     Color cor = new Color(img.getRGB(i, j));
                     int tomCinza = (int) ((0.299*cor.getRed() + 0.587*cor.getGreen() + 0.114*cor.getBlue()));
                     img.setRGB(i,j,tomCinza);
-
+                    matrizCinza[i][j]= tomCinza;
                 }
                 System.out.println("");
             }
 
             for (int i = 0; i<largura; i++){
                 for (int j = 0; j<altura; j++){
-                    System.out.print(img.getRGB(i,j) + " ");
+                    System.out.print(matrizCinza[i][j] + " ");
                 }
                 System.out.println(" ");
             }
