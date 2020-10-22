77f5e3c38d61ffcee268f834689bedc459c25fcb
Thuize Thainá <thuize@ufrn.edu.br>
2020-10-22 00:42:39 -0300

    Criando classe imagem

--- src/App.java
+++ src/App.java
@@ -1,10 +1,8 @@
+import classes.Imagem;
 import classes.MatrizParaImg;
-import jdk.swing.interop.SwingInterOpUtils;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 
@@ -12,31 +10,21 @@ public class App {
     public static void main(String[] args) {
 
         try {
-            String pathImg = "C:\\Users\\Programação\\Desktop\\Threads - Projeto\\Projeto-Threads\\src\\balloons.png";
+            String pathImg = "/home/thuize/Documentos/UFRN/Projeto-Threads/src/balloons.png";
+            //String pathImg = "C:\\Users\\Programação\\Desktop\\Threads - Projeto\\Projeto-Threads\\src\\balloons.png";
             BufferedImage img = ImageIO.read(new File(pathImg));
-            int largura = img.getWidth();
-            int altura = img.getHeight();
-            int[][] matrizCinza = new int[largura][altura];
-            for (int i = 0; i < largura; i++) {
-                for (int j = 0; j < altura; j++) {
-                    Color cor = new Color(img.getRGB(i, j));
-                    int tomCinza = (int) ((cor.getRed()));
-                    //img.setRGB(i, j, tomCinza);
-                    matrizCinza[i][j] = tomCinza;
-                }
-                System.out.println("");
-            }
 
-            for (int i = 0; i < largura; i++) {
-                for (int j = 0; j < altura; j++) {
-                    System.out.print(matrizCinza[i][j] + " ");
-                }
-                System.out.println(" ");
-            }
+            //Criando um objeto imagem
+            Imagem imagem = new Imagem(img);
+
+            //Gerando a matriz de cinza e exibindo
+            int[][] matrizCinza = imagem.getMatrizCinza(img);
+            imagem.printMatrizCinza(matrizCinza);
             
             MatrizParaImg matrizParaImg = new MatrizParaImg();
             BufferedImage imgOut = matrizParaImg.setPixelEscalaDeCinza(matrizCinza);
-            ImageIO.write(imgOut, "jpg", new File("C:\\Users\\Programação\\Desktop\\Threads - Projeto\\Projeto-Threads\\src\\out.jpg"));
+            ImageIO.write(imgOut, "jpg", new File("/home/thuize/Documentos/UFRN/Projeto-Threads/src/balloons.png"));
+            //ImageIO.write(imgOut, "jpg", new File("C:\\Users\\Programação\\Desktop\\Threads - Projeto\\Projeto-Threads\\src\\out.jpg"));
             System.out.println("Imagem criada");
 
         } catch (IOException e) {
---
+++ src/classes/Imagem.java
@@ -0,0 +1,77 @@
+package classes;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * Classe que representa uma imagem e suas propriedades
+ */
+public class Imagem {
+
+    /**
+     * Construtor da classe imagem
+     * @param img
+     */
+    public Imagem(BufferedImage img) {
+        setLargura(img.getWidth());
+        setAltura(img.getHeight());
+    }
+
+    /**
+     * Representa a largura da imagem
+     */
+    private int largura;
+
+    /**
+     * Representa a altura da imagem
+     */
+    private int altura;
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public void setLargura(int largura) {
+        this.largura = largura;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    /**
+     * Método que retorna a matriz de cinza a partir do objeto BufferedImage fornecido
+     * @param img
+     * @return matrizCinza
+     */
+    public int[][] getMatrizCinza(BufferedImage img){
+        int[][] matrizCinza = new int[this.largura][this.altura];
+        for (int i = 0; i < this.largura; i++) {
+            for (int j = 0; j < this.altura; j++) {
+                Color cor = new Color(img.getRGB(i, j));
+                int tomCinza = cor.getRed();
+                matrizCinza[i][j] = tomCinza;
+            }
+            System.out.println("");
+        }
+
+        return matrizCinza;
+    }
+
+    /**
+     * Metodo de exibe a matriz cinza passada como parametro
+     * @param matrizCinza
+     */
+    public void printMatrizCinza(int[][] matrizCinza){
+        for (int i = 0; i < this.largura; i++) {
+            for (int j = 0; j < this.altura; j++) {
+                System.out.print(matrizCinza[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
